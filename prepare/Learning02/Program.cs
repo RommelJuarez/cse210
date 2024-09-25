c297d5965010dab5d7b3824036d3fe36c68c6556
Rommel <cap03487@gmail.com>
2024-09-24 19:39:48 -0500

    Learning Activity week 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,38 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+
+        Job job1=new Job();
+
+        job1._jobTitle="Software Engineer";
+        job1._company="Apple";
+        job1._startYear="2022";
+        job1._endYear="2024";
+
+        Job job2=new Job();
+
+        job2._jobTitle="Astronaut";
+        job2._company="NASA";
+        job2._startYear="2019";
+        job2._endYear="2021";
+
+        
+        
+        Resume resume1=new Resume();
+
+        resume1._name="Rommel Juarez";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
+        
+
+        
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,14 @@
+public class Resume(){
+
+    public string _name="";
+    public List<Job> _jobs= new List<Job>();
+
+    public void Display(){
+        Console.WriteLine($"{_name}");
+        foreach (Job j in _jobs)
+        {
+            j.ShowJobInformation();
+        };
+
+    }
+}
