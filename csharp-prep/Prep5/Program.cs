e151aefe0acefed80b267fa277a92a5ee4f6af37
Rommel <cap03487@gmail.com>
2024-09-25 23:36:58 -0500

    fix the curly brace error

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,24 +2,24 @@ using System;
 
 class Program
 
-     
+{
+
     static void Main(string[] args)
-    {
-        // DisplayWelcome - Displays the message, "Welcome to the Program!" **
-        // PromptUserName - Asks for and returns the user's name (as a string) **
-        // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)**
-        // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)**
-        // DisplayResult - Accepts the user's name and the squared number and displays them. 
-        
-       
-        static void Main(string[] args)
-    {
-        DisplayWelcome();
-        string userName = PromptUserName();
-        int favNumber = PromptUserNumber();
-        int squaredNumber = SquareNumber(favNumber);
-        DisplayResult(userName, squaredNumber);
-    }
+{
+    // DisplayWelcome - Displays the message, "Welcome to the Program!" **
+    // PromptUserName - Asks for and returns the user's name (as a string) **
+    // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)**
+    // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)**
+    // DisplayResult - Accepts the user's name and the squared number and displays them. 
+
+
+
+    DisplayWelcome();
+    string userName = PromptUserName();
+    int favNumber = PromptUserNumber();
+    int squaredNumber = SquareNumber(favNumber);
+    DisplayResult(userName, squaredNumber);
+
 
     static void DisplayWelcome()
     {
@@ -50,4 +50,6 @@ class Program
     {
         Console.WriteLine($"{userName}, the square of your number is: {squaredNumber}");
     }
+}
+
 }
